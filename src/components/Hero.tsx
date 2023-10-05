7ece206aec4257d58b1d8deedf434e841c421945
Jorge Rodrigues <a20632@alunos.ipca.pt>
2023-10-05 11:19:27 +0100

    feat: Sphere in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Navbar from "./Navbar";
+import { MeshDistortMaterial, OrbitControls, Sphere } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 const Section = styled.div`
   height: 100vh;
@@ -64,6 +66,7 @@ position: absolute;
 top: 0;
 left: 0;
 right: 0;
+padding-top: 85px;
 margin: auto;
 animation: animate 2s infinite ease alternate;
 @keyframes animate {
@@ -89,6 +92,19 @@ const Hero = () => {
           <Button>Learn More</Button>
         </Left>
         <Right>
+          <Canvas>
+            <OrbitControls enableZoom={false} />
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[3, 2, 1]} />
+            <Sphere args={[1, 100, 200]} scale={2.3}>
+              <MeshDistortMaterial
+                color="#8548b3"
+                attach="material"
+                distort={0.5}
+                speed={2}
+              />
+            </Sphere>
+          </Canvas>
           <Img src="../../public/img/avataaars (1).png "/>
         </Right>
       </Container>
