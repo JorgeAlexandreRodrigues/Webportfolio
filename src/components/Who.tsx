f6806fcf323c0d9db1e7c26fbb9609790cb65e53
Jorge Rodrigues <a20632@alunos.ipca.pt>
2023-10-05 10:59:21 +0100

    feature:Cube component

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Cube from "./Cube";
 
 const Section = styled.div`
   height: 100vh;
@@ -58,6 +61,12 @@ const Who = () => {
     <Section>
       <Container>
         <Left>
+          <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[3, 2, 1]} />
+            <Cube />
+            <OrbitControls enableZoom={false} autoRotate />
+          </Canvas>
         </Left>
         <Right>
           <Title>Think outside the square space</Title>
